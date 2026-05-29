99f604281a948d3cd303a13d49a3f7b51c45bdf6
sabcaa <shawna.blumenschein@acupuncturealberta.ca>
2026-05-29 15:50:51 -0600

    Update exams.php

--- hello-elementor-child/exams.php
+++ hello-elementor-child/exams.php
@@ -18,7 +18,34 @@ get_header(); ?>
 <div class="page-layout">
 
  <main class="main-content">
-     <section class="content-section">
+
+ <!-- End of practicals exam transitions -->
+    <section class="content-section>
+        <h2>
+            <span class="section-icon"><i class="fa-solid fa-exclamation"></i></span>
+            Changes to exams effective June 2026
+        </h2>
+
+        <div class="callout">
+                On April 29, 2026, the Council approved termination of the practical components of the Alberta Acupuncture Registration Examinations (AARE). New candidates who have never attempted the AARE <strong>are not required</strong> to take or pass the acupuncture points or modalities exams. April 2026 was the last general administration of the practical exams.
+        </div>
+
+        <h3>Impact for current students who have never written the AARE</h3>
+        <p>Students who have never written the AARE are subject to the new policy and registration requirements. To register with the College, you will have to meet the registration requirements in place at the time you apply. As of June 2026, those requirements include passing the Pan-Canadian Examination and completing the College's online safety module.</p>
+
+        <h3>Impact for Alberta graduates who passed the AARE</h3>
+        <p>Candidates who passed all components of the AARE may proceed with registration.</p>
+
+        <h3>Impact for Alberta graduates who failed a practical component of the AARE</h3>
+        <p>Candidates who failed the acupuncture points or modalities exam, or both, remain subject to the registration requirements and policy that were in effect at the time they wrote the AARE. </p>
+        <p>Any candidates who wrote the AARE in April 2026 or prior and failed any component must still pass all components of the AARE. The College is providing a one-year transition period to support candidates in meeting their exam requirements.</p>
+
+
+    </section>
+
+<!-- regular page content -->
+
+    <section class="content-section">
         <h2>
             <span class="section-icon"><i class="fa-solid fa-stamp"></i></span>
             Approved registration exams
@@ -26,41 +53,27 @@ get_header(); ?>
 
         <p>The Council approves registration exams that may be used as an entry-to-practice exam in Alberta. The following exams are approved:</p>
             <ul>
-                <li>Pan-Canadian Acupuncturists Examination administered by CARB-TCMPA</li>
+                <li>Alberta Acupuncture Registration Examinations (AARE)</li>
                 <li>Acupuncture Certification of the National Certification Board for Acupuncture and Herbal Medicine exam</li>
                 <li>California Acupuncture Licensing Examination </li>
             </ul>
 
         <div class="callout-warning">
-                <strong>Important:</strong> An Alberta-based applicant who writes the Acupuncture Certification of the National Certification Board for Acupuncture and Herbal Medicine exam through the College <strong>does not</strong> gain the ability to be licensed or work in the United States. 
+                <strong>Important:</strong> An Alberta-based applicant who writes the Acupuncture Certification of the National Certification Board for Acupuncture and Herbal Medicine through the College <strong>does not</strong> gain the ability to be licensed or work in the United States. 
             </div>
 
      </section>
 
-     
-     <section class="content-section">
-        <h2>
-            <span class="section-icon"><i class="fa-solid fa-check"></i></span>
-            Eligibility for exams
-        </h2>
-
-        <p>Applicants educated in Alberta can apply for examinations if:</p>
-        <ul>
-            <li>The applicant has obtained a diploma from a program in acupuncture; OR</li>
-            <li>At the time of application, the applicant is enrolled in and actively completing the final year of their acupuncture education.</li>
-        </ul>
-        <p><strong>A</strong><strong>pplicants educated in a Canadian jurisdiction outside of Alberta</strong>&nbsp;must upload a copy of their diploma and transcripts to their online application form. Additional documentation, such as a program syllabus, may be requested.</p>
-        
-        <p><strong>International applicants</strong>&nbsp;must upload an education credential assessment from a&nbsp;<a href="https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/express-entry/documents/education-assessed/how.html" target="_blank" rel="noopener">designated organization</a>&nbsp;and copy of their diploma to their online application form. The education credential assessment must indicate a minimum 3 years of training in Acupuncture, Traditional Chinese Medicine or Medicine with a focus in Chinese Medicine. Additional documentation may be requested.</p>
-       
-        <p>More information on eligible credential assessments can be found in the International Graduates section on our&nbsp;<a href="https://www.acupuncturealberta.ca/applicants/applications/">Applications page</a>.</p>
-     </section>
-
+    
      <section class="content-section">
          <h2>
             <span class="section-icon"><i class="fa-solid fa-circle-info"></i></span>
-            Pan-Canadian exam information
+            Alberta Acupuncture Registration Examinations information
         </h2>
+        <p>The Alberta Acupuncture Registration Examinations includes the following components: 
+            <ul>
+                <li>Pan-Canadian Acupuncturists Examination administered by CARB-TCMPA</li>
+                <li>Online Safety Module offered by the College
         <p>For the most current and accurate information regarding the Pan-Canadian Examinations, refer directly to the&nbsp;<a href="https://carb-tcmpa.org/">CARB-TCMPA website</a>&nbsp;and communicate with CARB-TCMPA for any inquiries.</p>
         <h3>Important dates</h3>
         <table>
@@ -90,7 +103,26 @@ get_header(); ?>
         <h3>Exam withdrawal</h3>
         <p>Applicants who withdraw after the examination fees due date may not receive a full refund. Applicants must contact CARB-TCMPA if their withdrawal is within four weeks of the exam administration date.</p>
 
-         </section>
+    </section>
+     
+     <section class="content-section">
+        <h2>
+            <span class="section-icon"><i class="fa-solid fa-check"></i></span>
+            Eligibility for exams
+        </h2>
+
+        <p>Applicants educated in Alberta can apply for examinations if:</p>
+        <ul>
+            <li>The applicant has obtained a diploma from a program in acupuncture; OR</li>
+            <li>At the time of application, the applicant is enrolled in and actively completing the final year of their acupuncture education.</li>
+        </ul>
+        <p><strong>Applicants educated in a Canadian jurisdiction outside of Alberta</strong>&nbsp;must upload a copy of their diploma and transcripts to their online application form. Additional documentation, such as a program syllabus, may be requested.</p>
+        
+        <p><strong>International applicants</strong>&nbsp;must upload an education credential assessment from a&nbsp;<a href="https://www.canada.ca/en/immigration-refugees-citizenship/services/immigrate-canada/express-entry/documents/education-assessed/how.html" target="_blank" rel="noopener">designated organization</a>&nbsp;and copy of their diploma to their online application form. The education credential assessment must indicate a minimum 3 years of training in Acupuncture, Traditional Chinese Medicine or Medicine with a focus in Chinese Medicine. Additional documentation may be requested.</p>
+       
+        <p>More information on eligible credential assessments can be found in the International Graduates section on our&nbsp;<a href="https://www.acupuncturealberta.ca/applicants/applications/">Applications page</a>.</p>
+     </section>
+
 
    </main>
 </div>
